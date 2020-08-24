a066e7442ce27087bd64cffd9db4068b3568143d
ustrojny <ustrojny@gmail.com>
2020-08-25 00:09:44 +0200

    Updates

---
+++ precache-manifest.952dfe7865793efe03e00a732e7fa912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796b71b9707f502ae4a7db4d49b9608c",
+    "url": "/dogs-filter/index.html"
+  },
+  {
+    "revision": "04ecef73818482388603",
+    "url": "/dogs-filter/static/css/main.90636e81.chunk.css"
+  },
+  {
+    "revision": "cfec3b686c16fd75b532",
+    "url": "/dogs-filter/static/js/2.4dd40ea6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dogs-filter/static/js/2.4dd40ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ecef73818482388603",
+    "url": "/dogs-filter/static/js/main.b97c619e.chunk.js"
+  },
+  {
+    "revision": "28c84195d7cf323f046b",
+    "url": "/dogs-filter/static/js/runtime-main.cf7ef5ad.js"
+  }
+]);
