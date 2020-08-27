4130e8044637f438ce0c4d7dc817c1d9e3fd7f33
ustrojny <ustrojny@gmail.com>
2020-08-27 02:11:27 +0200

    Add tests with Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
@@ -12,5 +12,59 @@ test('placeholder for search', () => {
   const { getByPlaceholderText } = render(<App />);
   const placeholderElement = getByPlaceholderText(/wyszukaj/i);
   expect(placeholderElement).toBeInTheDocument();
+});
+
+*/
+
+///
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import App from './App';
+import DogsList from './DogsList';
+
+it('renders without crashing', () => {
+    shallow(<App />);
+});
+
+it('includes input', () => {
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement(<input />)).toEqual(true)
+});
+
+it('includes DogsList', () => {
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement(<DogsList />)).toEqual(true)
+});
+
+it('shows placeholder for search', () =>{
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement(/wyszukaj/i)).toEqual(true)
+});
+
+it('shows message when there are no dogs', () => {
+  const dogsList = shallow(<DogsList dogs={[]} />);
+  expect(dogsList.text()).toContain('Brak wyników!')
+});
+
+it('does not show message when there are dogs', () => {
+  const dogsList = shallow(<DogsList dogs={['Husky']} />);
+  expect(dogsList.text()).not.toContain('Brak wyników!')
+});
+
+it('shows a list of dogs', () => {
+  const dogs = ['Husky', 'Elkhund szary'];
+  const dogsList = shallow(<DogsList dogs={dogs} />);
+  expect(dogsList.find('li').length).toEqual(dogs.length);
+});
+
+describe('list of dogs', () => {
+  const dogs = ['Husky', 'Elkhund szary'];
+  const dogsList = shallow(<DogsList dogs={dogs} />);
+
+  dogs.forEach(dog => {
+    it(`includes name ${dog} on the list`, () => {
+      expect(dogsList.containsMatchingElement(<li>{dog}</li>)).toEqual(true)
+    });
+  });
 });
 
